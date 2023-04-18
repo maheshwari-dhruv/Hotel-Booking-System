7ec96a2e0c57fda4409cab50d2672aa5ab69b89e
Dhruv Maheshwari <mrdm026@gmail.com>
2023-04-18 12:44:49 +0530

    Moved project to main folder

---
+++ admin-service/src/main/java/hotel/booking/system/enums/Role.java
@@ -0,0 +1,7 @@
+package hotel.booking.system.enums;
+
+public enum Role {
+    ADMIN,
+    STAFF,
+    CUSTOMER
+}
---
+++ staff-service/src/main/java/hotel/booking/system/Main.java
@@ -0,0 +1,7 @@
+package hotel.booking.system;package hotel.booking.system;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
